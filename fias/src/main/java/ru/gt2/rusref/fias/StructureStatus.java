df486a41cf2bdd6bebcc0bb4c41d054e00eb6f70
ragesteel <devnull@localhost>
2012-03-24 08:32:45 +0400

    Начало работы над Признаком Строениея для ФИАС

---
+++ fias/src/main/java/ru/gt2/rusref/fias/StructureStatus.java
@@ -0,0 +1,18 @@
+package ru.gt2.rusref.fias;
+
+/**
+ * Признак строения.
+ *
+ * @author rage
+ */
+public class StructureStatus {
+    /** Признак строения */
+    private Integer strStatId;
+    
+    /** Наименование */
+    private String name;
+    
+    /** Краткое наименование */
+    private String shortName;
+    
+}
