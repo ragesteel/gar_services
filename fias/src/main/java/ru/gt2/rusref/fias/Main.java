3af51e9aece1c678ab4357e4c9e1f4c4f95a88b3
ragesteel <devnull@localhost>
2012-03-25 17:39:41 +0400

    Ещё одно FIXME

--- fias/src/main/java/ru/gt2/rusref/fias/Main.java
+++ fias/src/main/java/ru/gt2/rusref/fias/Main.java
@@ -14,6 +14,7 @@ import java.nio.file.Files;
  * FIXME Реализовать свой конвертор и валидатор для GUID.
  * FIXME Включить валидацию с использованием BeanValidation.
  * FIXME Сделать все типы сериализуемыми.
+ * FIXME Проверка конвертации путём обратного преобразования в xml-строку.
  */
 public class Main {
     private static final String FILE_PREFIX = "AS_";
