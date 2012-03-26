1bd5cd7e8838f50d8332b3727e095f145a2afb00
ragesteel <devnull@localhost>
2012-03-26 22:31:19 +0300

    Добавление информации по вложенным типам

--- fias/src/main/java/ru/gt2/rusref/fias/Fias.java
+++ fias/src/main/java/ru/gt2/rusref/fias/Fias.java
@@ -4,25 +4,28 @@ package ru.gt2.rusref.fias;
  * Все файлы ФИАС.
  */
 public enum Fias {
-    STRSTAT(StructureStatuses.class),
-    ACTSTAT(ActualStatuses.class),
-    INTVSTAT(IntervalStatuses.class),
-    ESTSTAT(EstateStatuses.class),
-    CENTERST(CenterStatuses.class),
-    OPERSTAT(OperationStatuses.class),
-    HSTSTAT(HouseStateStatuses.class),
-    CURENTST(CurrentStatuses.class),
-    SOCRBASE(AddressObjectTypes.class),
-    LANDMARK(Landmarks.class),
-    HOUSEINT(HouseIntervals.class),
-    NORMDOC(NormativeDocumentes.class),
-    ADDROBJ(AddressObjects.class),
-    HOUSE(Houses.class);
+    STRSTAT(StructureStatuses.class, StructureStatus.class),
+    ACTSTAT(ActualStatuses.class, ActualStatus.class),
+    INTVSTAT(IntervalStatuses.class, IntervalStatus.class),
+    ESTSTAT(EstateStatuses.class, EstateStatus.class),
+    CENTERST(CenterStatuses.class, CenterStatus.class),
+    OPERSTAT(OperationStatuses.class, OperationStatus.class),
+    HSTSTAT(HouseStateStatuses.class, HouseStateStatus.class),
+    CURENTST(CurrentStatuses.class, CurrentStatus.class),
+    SOCRBASE(AddressObjectTypes.class, AddressObjectType.class),
+    LANDMARK(Landmarks.class, Landmark.class),
+    HOUSEINT(HouseIntervals.class, HouseInterval.class),
+    NORMDOC(NormativeDocumentes.class, NormativeDocument.class),
+    ADDROBJ(AddressObjects.class, AddressObject.class),
+    HOUSE(Houses.class, House.class);
 
     /** Класс обёртки. */
     public final Class<?> wrapper;
+    /** Внутренний класс. */
+    public final Class<?> item;
     
-    private Fias(Class<?> wrapper) {
+    private Fias(Class<?> wrapper, Class<?> item) {
         this.wrapper = wrapper;
+        this.item = item;
     }
 }
