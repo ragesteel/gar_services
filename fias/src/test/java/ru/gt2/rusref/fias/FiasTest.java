7e85fe4a46fd0e04d91aba726bc22e99d7f11f16
ragesteel <devnull@localhost>
2012-03-26 23:02:50 +0300

    Начало работы над тестами

---
+++ fias/src/test/java/ru/gt2/rusref/fias/FiasTest.java
@@ -0,0 +1,17 @@
+package ru.gt2.rusref.fias;
+
+import org.junit.Test;
+
+/**
+ * Тест для проверки соответствия аннотаций моим пожеланиям к классам.
+ */
+public class FiasTest {
+    /**
+     * Проверка налаичия всех полей в propOrder и propOrder на укакзания на поля.
+     * Дубликаты propOrder также нужно удалять.
+     */
+    @Test
+    public void testFieldsInPropOrder() {
+
+    }
+}
