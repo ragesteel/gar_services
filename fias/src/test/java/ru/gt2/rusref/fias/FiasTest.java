84a1b521b66a67d65d2145caf653d664b7a973bc
ragesteel <devnull@localhost>
2012-03-27 08:32:22 +0300

    Начало работы над ограничениями для типов в тестах

--- fias/src/test/java/ru/gt2/rusref/fias/FiasTest.java
+++ fias/src/test/java/ru/gt2/rusref/fias/FiasTest.java
@@ -2,6 +2,8 @@ package ru.gt2.rusref.fias;
 
 import com.google.common.base.Function;
 import com.google.common.base.Functions;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Iterators;
 import com.google.common.collect.Lists;
@@ -11,18 +13,41 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import javax.annotation.Nullable;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.Future;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlType;
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 import java.util.Set;
+import java.util.UUID;
 
 /**
  * Тест для проверки соответствия аннотаций моим пожеланиям к классам.
  */
 public class FiasTest {
+
+    private static Function<Field, String> FIELD_NAME = new Function<Field, String>() {
+        @Override
+        public String apply(@Nullable Field field) {
+            return field.getName();
+        }
+    };
+    
+    private static final ImmutableMap<Class<?>, ImmutableSet<Class<? extends Annotation>>> CONSTRAINS_BY_TYPES =
+        ImmutableMap.<Class<?>, ImmutableSet<Class<? extends Annotation>>>builder()
+            .put(String.class, ImmutableSet.of(NotNull.class, Size.class))
+            .put(Integer.class, ImmutableSet.of(NotNull.class, Digits.class))
+            .put(UUID.class, ImmutableSet.<Class<? extends Annotation>>of(NotNull.class))
+            .put(Date.class, ImmutableSet.of(NotNull.class, Past.class, Future.class))
+            .build();
+
     /**
      * Проверка налаичия всех полей в propOrder и propOrder на укакзания на поля.
      * Дубликаты propOrder также нужно удалять.
@@ -35,7 +60,7 @@ public class FiasTest {
     }
 
     /**
-     * Проверка на то, что в каждом поле есть @NotNull, если но Required
+     * Проверка на то, что в каждом поле есть @NotNull, если но Required.
      */
     @Test
     public void testFieldsNullable() {
@@ -44,21 +69,29 @@ public class FiasTest {
         }
     }
 
+    /**
+     * Проверка ограничений по полям, в зависмости от типа.
+     */
+    @Test
+    public void testFieldConstrainsByType() {
+        for (Fias fias : Fias.values()) {
+            testFieldsConstrainsByType(fias);
+        }
+    }
+
     private void testFieldsInPropOrder(Fias fias) {
-        Class<?> item = fias.item;
-        String[] propOrderArr = getPropOrder(item);
+        String[] propOrderArr = getPropOrder(fias);
         Set<String> propOrder = Sets.newHashSet(propOrderArr);
         Assert.assertEquals(propOrderArr.length, propOrder.size());
         
-        List<Field> fields = getAllFields(item);
+        List<Field> fields = getAllFields(fias);
         Set<String> fieldNames = Sets.newHashSet(Iterables.transform(fields, FIELD_NAME));
         Assert.assertEquals(fields.size(), fieldNames.size());
         Assert.assertEquals(fias.name(), fieldNames, propOrder);
     }
 
     private void testFieldsNullable(Fias fias) {
-        Class<?> item = fias.item;
-        List<Field> fields = getAllFields(item);
+        List<Field> fields = getAllFields(fias);
         for (Field field : fields) {
             XmlAttribute xmlAttribute = field.getAnnotation(XmlAttribute.class);
             Assert.assertNotNull(xmlAttribute);
@@ -71,13 +104,33 @@ public class FiasTest {
         }
     }
 
-    private String[] getPropOrder(Class<?> item) {
+    private void testFieldsConstrainsByType(Fias fias) {
+        List<Field> fields = getAllFields(fias);
+        for (Field field : fields) {
+            Class<?> type = field.getType();
+            ImmutableSet<Class<? extends Annotation>> constrains = CONSTRAINS_BY_TYPES.get(type);
+            Assert.assertNotNull("Type " + type + " does not contains in supported types",
+                    constrains);
+            
+            System.out.println(field.getAnnotations());
+            
+            
+
+            
+        }
+    }
+    
+    // internals
+
+    private String[] getPropOrder(Fias fias) {
+        Class<?> item = fias.item;
         XmlType xmlType = item.getAnnotation(XmlType.class);
         Assert.assertNotNull(xmlType);
         return xmlType.propOrder();
     }
     
-    private List<Field> getAllFields(Class<?> item) {
+    private List<Field> getAllFields(Fias fias) {
+        Class<?> item = fias.item;
         List<Field> fields = Lists.newArrayList();
         while (!Object.class.equals(item)) {
             fields.addAll(Arrays.asList(item.getDeclaredFields()));
@@ -86,10 +139,4 @@ public class FiasTest {
         return fields;
     }
 
-    private static Function<Field, String> FIELD_NAME = new Function<Field, String>() {
-        @Override
-        public String apply(@Nullable Field field) {
-            return field.getName();
-        }
-    };
 }
