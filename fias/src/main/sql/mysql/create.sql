37147ee74b2321175a97c9fad3b08720f7e1aa5c
ragesteel <devnull@localhost>
2012-04-04 22:00:07 +0400

    Начало работы над генерацией структуры базы.

--- fias/src/main/sql/mysql/create.sql
+++ fias/src/main/sql/mysql/create.sql
@@ -1,4 +1,11 @@
 CREATE DATABASE IF NOT EXISTS fias
     COLLATE 'utf8_unicode_ci';
 
+CREATE TABLE `StrStat` (
+  `strStatId` INT(10) NOT NULL COMMENT "Признак строения",
+  `name` VARCHAR(20) COMMENT "Наименование",
+  `shortName` VARCHAR(20) COMMENT "Краткое наименование",
+  PRIMARY KEY (`strStatId`)
+) COMMENT 'Признак строения';
+
 
