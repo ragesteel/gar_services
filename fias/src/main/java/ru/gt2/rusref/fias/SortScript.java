bf33d6a9c89f43f05ccde05e3f7790257c18fcdb
ragesteel <devnull@localhost>
2012-04-19 07:16:07 +0400

    Добавление в скрипт сортировки для *nix операционных систем

--- fias/src/main/java/ru/gt2/rusref/fias/SortScript.java
+++ fias/src/main/java/ru/gt2/rusref/fias/SortScript.java
@@ -2,11 +2,30 @@ package ru.gt2.rusref.fias;
 
 public class SortScript {
     public static void main(String... args) {
+        cmd();
+        shell();
+    }
+
+    private static void cmd() {
         for (Fias fias : Fias.orderForLoading()) {
+            if (fias.intermediate) {
+                continue;
+            }
             String name = fias.item.getSimpleName();
             System.out.println("move " + name + ".csv " + name + "-unsorted.csv");
             System.out.println("sort.exe " + name + "-unsorted.csv /output " + name + ".csv");
             System.out.println("del " + name + "-unsorted.csv");
         }
     }
+
+    private static void shell() {
+        for (Fias fias : Fias.orderForLoading()) {
+            if (fias.intermediate) {
+                continue;
+            }
+            String name = fias.item.getSimpleName();
+            System.out.println("sort " + name + ".csv --output=" + name + ".csv");
+        }
+    }
+
 }
