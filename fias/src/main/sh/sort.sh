92bf1ef9b75c023287df0ddfb52f3c1956c2a560
ragesteel <devnull@localhost>
2012-11-15 20:22:39 +0400

    Добавление скрипта для сортировки

---
+++ fias/src/main/sh/sort.sh
@@ -0,0 +1,15 @@
+﻿#!/bin/sh
+sort NormativeDocument.csv --output=NormativeDocument.csv
+sort Landmark.csv --output=Landmark.csv
+sort AddressObjectType.csv --output=AddressObjectType.csv
+sort CurrentStatus.csv --output=CurrentStatus.csv
+sort ActualStatus.csv --output=ActualStatus.csv
+sort OperationStatus.csv --output=OperationStatus.csv
+sort CenterStatus.csv --output=CenterStatus.csv
+sort AddressObject.csv --output=AddressObject.csv
+sort IntervalStatus.csv --output=IntervalStatus.csv
+sort HouseInterval.csv --output=HouseInterval.csv
+sort HouseStateStatus.csv --output=HouseStateStatus.csv
+sort EstateStatus.csv --output=EstateStatus.csv
+sort StructureStatus.csv --output=StructureStatus.csv
+sort House.csv --output=House.csv
