88c9160590731f06dca168c677c5d958210619f5
Rage Steel <ragesteel@gmail.com>
2025-09-02 15:15:07 +0300

    Начало работы над проектом импорта/парсинга данных.

---
+++ parse/src/main/java/ru/gt2/gar/parse/ParseApplication.java
@@ -0,0 +1,11 @@
+package ru.gt2.gar.parse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParseApplication {
+    public static void main(String... args) {
+        SpringApplication.run(ParseApplication.class, args);
+    }
+}
