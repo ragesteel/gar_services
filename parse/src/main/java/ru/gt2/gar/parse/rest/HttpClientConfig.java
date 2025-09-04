ac80f8237c93daa1110d3c07cd6341eb80b02e70
Rage Steel <ragesteel@gmail.com>
2025-09-04 15:29:47 +0300

    Убрал ненужное больше добавление USER_AGENT'а в RestClient.

--- parse/src/main/java/ru/gt2/gar/parse/rest/HttpClientConfig.java
+++ parse/src/main/java/ru/gt2/gar/parse/rest/HttpClientConfig.java
@@ -1,6 +1,5 @@
 package ru.gt2.gar.parse.rest;
 
-import com.google.common.net.HttpHeaders;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
@@ -11,9 +10,7 @@ import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 public class HttpClientConfig {
     @Bean
     public FileInfoService fileInfoServiceClient(RestClient.Builder restClientBuilder) {
-        RestClient restClient = restClientBuilder.baseUrl("https://fias.nalog.ru")
-                .requestInitializer(req -> req.getHeaders().add(HttpHeaders.USER_AGENT, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36"))
-                .build();
+        RestClient restClient = restClientBuilder.baseUrl("https://fias.nalog.ru").build();
         return HttpServiceProxyFactory.builderFor(RestClientAdapter.create(restClient)).build().createClient(FileInfoService.class);
     }
 }
