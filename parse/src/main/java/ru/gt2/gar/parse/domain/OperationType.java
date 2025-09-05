c1a6f5260922c0d8eee917b7bb389f86813224f6
Rage Steel <ragesteel@gmail.com>
2025-09-05 16:17:01 +0300

    Добавил javadoc — добавил читаемости параметрам пакете domain.

--- parse/src/main/java/ru/gt2/gar/parse/domain/OperationType.java
+++ parse/src/main/java/ru/gt2/gar/parse/domain/OperationType.java
@@ -6,14 +6,14 @@ import java.time.LocalDate;
  * Статус действия.
  * Основа — AS_OPERATION_TYPES_2_251_14_04_01_01.xsd
  *
- * @param id Идентификатор статуса (ключ)
- * @param name Наименование; Длина: от 1 до 100 символов
- * @param shortName Краткое наименование; Опциональное поле; Описание. Длина: от 0 до 100 символов
- * @param desc Описание; Опциональное поле
+ * @param id         Идентификатор статуса (ключ)
+ * @param name       Наименование; Длина: от 1 до 100 символов
+ * @param shortName  Краткое наименование; Опциональное поле; Длина: от 0 до 100 символов
+ * @param desc       Описание; Опциональное поле
  * @param updateDate Дата внесения (обновления) записи
- * @param startDate Начало действия записи
- * @param endDate Окончание действия записи
- * @param isActive Статус активности
+ * @param startDate  Начало действия записи
+ * @param endDate    Окончание действия записи
+ * @param isActive   Статус активности
  */
 public record OperationType(
         int id, 
