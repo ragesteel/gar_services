aba5da5682576caa5f4a5ee6d5aad55acb292b4f
ragesteel <ragesteel@gmail.com>
2026-03-03 18:22:37 +0300

    Feature(#7) Начало работы над отдельной иерахией классов для маппинга ГАР-сущностей.

---
+++ db/src/main/java/ru/gt2/gar/db/jrm/AddressObjectTypeJm.java
@@ -0,0 +1,52 @@
+package ru.gt2.gar.db.jrm;
+
+import ru.gt2.gar.domain.AddressObjectType;
+
+import java.sql.JDBCType;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Types;
+import java.time.LocalDate;
+
+/// Пример того, как я хочу видеть реализацию класса
+/// Везде используем порядковые номера
+/// Да, тут из-за использования LocalDate нужен JDBC 4.2+
+public class AddressObjectTypeJm implements JdbcMapper<AddressObjectType> {
+    @Override
+    public void write(AddressObjectType source, PreparedStatement ps) throws SQLException {
+        ps.setInt(1, source.id());
+        ps.setInt(2, source.level());
+        ps.setString(3, source.shortName());
+        ps.setString(4, source.name());
+        String desc = source.desc();
+        if (null == desc) {
+            ps.setNull(5, Types.VARCHAR);
+        } else {
+            ps.setString(5, desc);
+        }
+        ps.setObject(6, source.updateDate(), JDBCType.DATE);
+        ps.setObject(7, source.startDate(), JDBCType.DATE);
+        ps.setObject(8, source.endDate(), JDBCType.DATE);
+        ps.setBoolean(9, source.isActive());
+    }
+
+    @Override
+    public AddressObjectType read(ResultSet rs) throws SQLException {
+        int id = rs.getInt(1);
+        int level = rs.getInt(2);
+        String shortName = rs.getString(3);
+        String name = rs.getString(4);
+        String desc = rs.getString(5);
+        LocalDate updateDate = rs.getObject(6, LocalDate.class);
+        LocalDate startDate = rs.getObject(7, LocalDate.class);
+        LocalDate endDate = rs.getObject(8, LocalDate.class);
+        boolean isActive = rs.getBoolean(9);
+        return new AddressObjectType(id, level, shortName, name, desc, updateDate, startDate, endDate, isActive);
+    }
+
+    @Override
+    public int columnCount() {
+        return 9;
+    }
+}
---
+++ db/src/main/java/ru/gt2/gar/db/jrm/JdbcMapper.java
@@ -0,0 +1,16 @@
+package ru.gt2.gar.db.jrm;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public interface JdbcMapper<T> {
+    // Вызов ps.set…(#, source.(get)) для установки значений
+    void write(T source, PreparedStatement ps) throws SQLException;
+
+    // Чтение из rs.get..(#) и последующее создание объекта
+    T read(ResultSet rs) throws SQLException;
+
+    // Вернуть количество ожидаемых колонок, можно использовать для проверок соответствия
+    int columnCount();
+}
