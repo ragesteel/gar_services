e2d92248143a5381d00840d78551400230cf9847
Rage Steel <ragesteel@gmail.com>
2026-03-05 23:38:49 +0300

    Feature(#9) Убрал параметр класса из XMLAttrMapper, ибо он уже есть в GarType. Ну и убрал параметризацию из самого класса XMLAttrMapper.

--- parse/src/main/java/ru/gt2/gar/parse/xml/XMLAttrMapper.java
+++ parse/src/main/java/ru/gt2/gar/parse/xml/XMLAttrMapper.java
@@ -1,28 +1,6 @@
 package ru.gt2.gar.parse.xml;
 
-import ru.gt2.gar.domain.AddressObject;
-import ru.gt2.gar.domain.AddressObjectDivision;
-import ru.gt2.gar.domain.AddressObjectType;
-import ru.gt2.gar.domain.AdmHierarchy;
-import ru.gt2.gar.domain.Apartment;
-import ru.gt2.gar.domain.ApartmentType;
-import ru.gt2.gar.domain.CarPlace;
-import ru.gt2.gar.domain.ChangeHistory;
 import ru.gt2.gar.domain.GarType;
-import ru.gt2.gar.domain.House;
-import ru.gt2.gar.domain.HouseType;
-import ru.gt2.gar.domain.MunHierarchy;
-import ru.gt2.gar.domain.NormativeDoc;
-import ru.gt2.gar.domain.NormativeDocKind;
-import ru.gt2.gar.domain.NormativeDocType;
-import ru.gt2.gar.domain.ObjectLevel;
-import ru.gt2.gar.domain.OperationType;
-import ru.gt2.gar.domain.Param;
-import ru.gt2.gar.domain.ParamType;
-import ru.gt2.gar.domain.ReestrObject;
-import ru.gt2.gar.domain.Room;
-import ru.gt2.gar.domain.RoomType;
-import ru.gt2.gar.domain.Stead;
 
 import java.util.function.BiFunction;
 
@@ -31,77 +9,75 @@ import static ru.gt2.gar.parse.xml.AttrConverter.BOOL_ACTUAL_ACTIVE;
 import static ru.gt2.gar.parse.xml.AttrConverter.NOTHING;
 
 // TODO Придумать более правильное название для этого класса, какая-нибудь GarParseData
-public class XMLAttrMapper<T> {
-    public static final XMLAttrMapper<AddressObject> ADDRESS_OBJECT = new XMLAttrMapper<>(
-            AddressObject.class, GarType.ADDR_OBJ, BOOL_ACTUAL_ACTIVE);
-    public static final XMLAttrMapper<AddressObjectDivision> ADDRESS_OBJECT_DIVISION = new XMLAttrMapper<>(
-            AddressObjectDivision.class, GarType.ADDR_OBJ_DIVISION);
-    public static final XMLAttrMapper<AddressObjectType> ADDRESS_OBJECT_TYPE = new XMLAttrMapper<>(
-            AddressObjectType.class, GarType.ADDR_OBJ_TYPES);
-    public static final XMLAttrMapper<AdmHierarchy> ADM_HIERARCHY = new XMLAttrMapper<>(
-            AdmHierarchy.class, GarType.ADM_HIERARCHY, BOOL_ACTIVE);
-    public static final XMLAttrMapper<ApartmentType> APARTMENT_TYPE = new XMLAttrMapper<>(
-            ApartmentType.class, GarType.APARTMENT_TYPES);
-    public static final XMLAttrMapper<Apartment> APARTMENT = new XMLAttrMapper<>(
-            Apartment.class, GarType.APARTMENTS, BOOL_ACTUAL_ACTIVE);
-    public static final XMLAttrMapper<OperationType> OPERATION_TYPE = new XMLAttrMapper<>(
-            OperationType.class, GarType.OPERATION_TYPES);
-    public static final XMLAttrMapper<CarPlace> CAR_PLACE = new XMLAttrMapper<>(
-            CarPlace.class, GarType.CARPLACES, BOOL_ACTUAL_ACTIVE);
-    public static final XMLAttrMapper<ChangeHistory> CHANGE_HISTORY = new XMLAttrMapper<>(
-            ChangeHistory.class, GarType.CHANGE_HISTORY);
-    public static final XMLAttrMapper<HouseType> HOUSE_TYPE = new XMLAttrMapper<>(
-            HouseType.class, GarType.HOUSE_TYPES);
-    public static final XMLAttrMapper<House> HOUSE = new XMLAttrMapper<>(
-            House.class, GarType.HOUSES, BOOL_ACTUAL_ACTIVE);
-    public static final XMLAttrMapper<MunHierarchy> MUN_HIERARCHY = new XMLAttrMapper<>(
-            MunHierarchy.class, GarType.MUN_HIERARCHY, BOOL_ACTIVE);
-    public static final XMLAttrMapper<NormativeDoc> NORMATIVE_DOC = new XMLAttrMapper<>(
-            NormativeDoc.class, GarType.NORMATIVE_DOCS);
-    public static final XMLAttrMapper<NormativeDocKind> NORMATIVE_DOC_KIND = new XMLAttrMapper<>(
-            NormativeDocKind.class, GarType.NORMATIVE_DOCS_KINDS);
-    public static final XMLAttrMapper<NormativeDocType> NORMATIVE_DOC_TYPE = new XMLAttrMapper<>(
-            NormativeDocType.class, GarType.NORMATIVE_DOCS_TYPES);
-    public static final XMLAttrMapper<ObjectLevel> OBJECT_LEVEL = new XMLAttrMapper<>(
-            ObjectLevel.class, GarType.OBJECT_LEVELS);
-    public static final XMLAttrMapper<ParamType> PARAM_TYPE = new XMLAttrMapper<>(
-            ParamType.class, GarType.PARAM_TYPES);
-    public static final XMLAttrMapper<Param> ADDR_OBJ_PARAM = new XMLAttrMapper<>(
-            Param.class, GarType.ADDR_OBJ_PARAMS);
-    public static final XMLAttrMapper<Param> HOUSES_PARAM = new XMLAttrMapper<>(
-            Param.class, GarType.HOUSES_PARAMS);
-    public static final XMLAttrMapper<Param> APARTMENTS_PARAM = new XMLAttrMapper<>(
-            Param.class, GarType.APARTMENTS_PARAMS);
-    public static final XMLAttrMapper<Param> ROOMS_PARAM = new XMLAttrMapper<>(
-            Param.class, GarType.ROOMS_PARAMS);
-    public static final XMLAttrMapper<Param> STEADS_PARAM = new XMLAttrMapper<>(
-            Param.class, GarType.STEADS_PARAMS);
-    public static final XMLAttrMapper<Param> CARPLACES_PARAM = new XMLAttrMapper<>(
-            Param.class, GarType.CARPLACES_PARAMS);
-    public static final XMLAttrMapper<ReestrObject> REESTR_OBJECT = new XMLAttrMapper<>(
-            ReestrObject.class, GarType.REESTR_OBJECTS, BOOL_ACTIVE);
-    public static final XMLAttrMapper<RoomType> ROOM_TYPE = new XMLAttrMapper<>(
-            RoomType.class, GarType.ROOM_TYPES);
-    public static final XMLAttrMapper<Room> ROOM = new XMLAttrMapper<>(
-            Room.class, GarType.ROOMS, BOOL_ACTUAL_ACTIVE);
-    public static final XMLAttrMapper<Stead> STEAD = new XMLAttrMapper<>(
-            Stead.class, GarType.STEADS, BOOL_ACTUAL_ACTIVE);
-    public static final XMLAttrMapper<HouseType> ADD_HOUSE_TYPE = new XMLAttrMapper<>(
-            HouseType.class, GarType.ADDHOUSE_TYPES);
+public class XMLAttrMapper {
+    public static final XMLAttrMapper ADDRESS_OBJECT = new XMLAttrMapper(
+            GarType.ADDR_OBJ, BOOL_ACTUAL_ACTIVE);
+    public static final XMLAttrMapper ADDRESS_OBJECT_DIVISION = new XMLAttrMapper(
+            GarType.ADDR_OBJ_DIVISION);
+    public static final XMLAttrMapper ADDRESS_OBJECT_TYPE = new XMLAttrMapper(
+            GarType.ADDR_OBJ_TYPES);
+    public static final XMLAttrMapper ADM_HIERARCHY = new XMLAttrMapper(
+            GarType.ADM_HIERARCHY, BOOL_ACTIVE);
+    public static final XMLAttrMapper APARTMENT_TYPE = new XMLAttrMapper(
+            GarType.APARTMENT_TYPES);
+    public static final XMLAttrMapper APARTMENT = new XMLAttrMapper(
+            GarType.APARTMENTS, BOOL_ACTUAL_ACTIVE);
+    public static final XMLAttrMapper OPERATION_TYPE = new XMLAttrMapper(
+            GarType.OPERATION_TYPES);
+    public static final XMLAttrMapper CAR_PLACE = new XMLAttrMapper(
+            GarType.CARPLACES, BOOL_ACTUAL_ACTIVE);
+    public static final XMLAttrMapper CHANGE_HISTORY = new XMLAttrMapper(
+            GarType.CHANGE_HISTORY);
+    public static final XMLAttrMapper HOUSE_TYPE = new XMLAttrMapper(
+            GarType.HOUSE_TYPES);
+    public static final XMLAttrMapper HOUSE = new XMLAttrMapper(
+            GarType.HOUSES, BOOL_ACTUAL_ACTIVE);
+    public static final XMLAttrMapper MUN_HIERARCHY = new XMLAttrMapper(
+            GarType.MUN_HIERARCHY, BOOL_ACTIVE);
+    public static final XMLAttrMapper NORMATIVE_DOC = new XMLAttrMapper(
+            GarType.NORMATIVE_DOCS);
+    public static final XMLAttrMapper NORMATIVE_DOC_KIND = new XMLAttrMapper(
+            GarType.NORMATIVE_DOCS_KINDS);
+    public static final XMLAttrMapper NORMATIVE_DOC_TYPE = new XMLAttrMapper(
+            GarType.NORMATIVE_DOCS_TYPES);
+    public static final XMLAttrMapper OBJECT_LEVEL = new XMLAttrMapper(
+            GarType.OBJECT_LEVELS);
+    public static final XMLAttrMapper PARAM_TYPE = new XMLAttrMapper(
+            GarType.PARAM_TYPES);
+    public static final XMLAttrMapper ADDR_OBJ_PARAM = new XMLAttrMapper(
+            GarType.ADDR_OBJ_PARAMS);
+    public static final XMLAttrMapper HOUSES_PARAM = new XMLAttrMapper(
+            GarType.HOUSES_PARAMS);
+    public static final XMLAttrMapper APARTMENTS_PARAM = new XMLAttrMapper(
+            GarType.APARTMENTS_PARAMS);
+    public static final XMLAttrMapper ROOMS_PARAM = new XMLAttrMapper(
+            GarType.ROOMS_PARAMS);
+    public static final XMLAttrMapper STEADS_PARAM = new XMLAttrMapper(
+            GarType.STEADS_PARAMS);
+    public static final XMLAttrMapper CARPLACES_PARAM = new XMLAttrMapper(
+            GarType.CARPLACES_PARAMS);
+    public static final XMLAttrMapper REESTR_OBJECT = new XMLAttrMapper(
+            GarType.REESTR_OBJECTS, BOOL_ACTIVE);
+    public static final XMLAttrMapper ROOM_TYPE = new XMLAttrMapper(
+            GarType.ROOM_TYPES);
+    public static final XMLAttrMapper ROOM = new XMLAttrMapper(
+            GarType.ROOMS, BOOL_ACTUAL_ACTIVE);
+    public static final XMLAttrMapper STEAD = new XMLAttrMapper(
+            GarType.STEADS, BOOL_ACTUAL_ACTIVE);
+    public static final XMLAttrMapper ADD_HOUSE_TYPE = new XMLAttrMapper(
+            GarType.ADDHOUSE_TYPES);
 
-    public final Class<T> valueClass;
     public final GarType garType;
     public final BiFunction<String, String, String> valueProcessing;
 
-    private XMLAttrMapper(Class<T> valueClass, GarType garType,
+    private XMLAttrMapper(GarType garType,
                           BiFunction<String, String, String> valueProcessing) {
-        this.valueClass = valueClass;
         this.garType = garType;
         this.valueProcessing = valueProcessing;
     }
 
-    private XMLAttrMapper(Class<T> valueClass, GarType garType) {
-        this(valueClass, garType, NOTHING);
+    private XMLAttrMapper(GarType garType) {
+        this(garType, NOTHING);
     }
 
     /*
