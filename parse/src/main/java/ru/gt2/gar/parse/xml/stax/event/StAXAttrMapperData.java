4f2814f7c25e4eb1382bba47373e559e28a5c4e1
ragesteel <ragesteel@gmail.com>
2026-03-09 12:36:58 +0300

    Feature(#9) Убрал статический инициализатор, теперь переменная инциализируется прямо в определении.

--- parse/src/main/java/ru/gt2/gar/parse/xml/stax/event/StAXAttrMapperData.java
+++ parse/src/main/java/ru/gt2/gar/parse/xml/stax/event/StAXAttrMapperData.java
@@ -19,40 +19,36 @@ public class StAXAttrMapperData {
     public final GarType garType;
     public final BiFunction<String, String, String> valueProcessing;
 
-    private static final Map<GarType, StAXAttrMapperData> FOR_TYPE;
-
-    static {
-        FOR_TYPE = Stream.of(
-                new StAXAttrMapperData(GarType.ADDR_OBJ, BOOL_ACTUAL_ACTIVE),
-                new StAXAttrMapperData(GarType.ADDR_OBJ_DIVISION),
-                new StAXAttrMapperData(GarType.ADDR_OBJ_TYPES),
-                new StAXAttrMapperData(GarType.ADM_HIERARCHY, BOOL_ACTIVE),
-                new StAXAttrMapperData(GarType.APARTMENT_TYPES),
-                new StAXAttrMapperData(GarType.APARTMENTS, BOOL_ACTUAL_ACTIVE),
-                new StAXAttrMapperData(GarType.OPERATION_TYPES),
-                new StAXAttrMapperData(GarType.CARPLACES, BOOL_ACTUAL_ACTIVE),
-                new StAXAttrMapperData(GarType.CHANGE_HISTORY),
-                new StAXAttrMapperData(GarType.HOUSE_TYPES),
-                new StAXAttrMapperData(GarType.HOUSES, BOOL_ACTUAL_ACTIVE),
-                new StAXAttrMapperData(GarType.MUN_HIERARCHY, BOOL_ACTIVE),
-                new StAXAttrMapperData(GarType.NORMATIVE_DOCS),
-                new StAXAttrMapperData(GarType.NORMATIVE_DOCS_KINDS),
-                new StAXAttrMapperData(GarType.NORMATIVE_DOCS_TYPES),
-                new StAXAttrMapperData(GarType.OBJECT_LEVELS),
-                new StAXAttrMapperData(GarType.PARAM_TYPES),
-                new StAXAttrMapperData(GarType.ADDR_OBJ_PARAMS),
-                new StAXAttrMapperData(GarType.HOUSES_PARAMS),
-                new StAXAttrMapperData(GarType.APARTMENTS_PARAMS),
-                new StAXAttrMapperData(GarType.ROOMS_PARAMS),
-                new StAXAttrMapperData(GarType.STEADS_PARAMS),
-                new StAXAttrMapperData(GarType.CARPLACES_PARAMS),
-                new StAXAttrMapperData(GarType.REESTR_OBJECTS, BOOL_ACTIVE),
-                new StAXAttrMapperData(GarType.ROOM_TYPES),
-                new StAXAttrMapperData(GarType.ROOMS, BOOL_ACTUAL_ACTIVE),
-                new StAXAttrMapperData(GarType.STEADS, BOOL_ACTUAL_ACTIVE),
-                new StAXAttrMapperData(GarType.ADDHOUSE_TYPES)
-        ).collect(Collectors.toMap(StAXAttrMapperData::getGarType, Functions.identity()));
-    }
+    private static final Map<GarType, StAXAttrMapperData> FOR_TYPE = Stream.of(
+            new StAXAttrMapperData(GarType.ADDR_OBJ, BOOL_ACTUAL_ACTIVE),
+            new StAXAttrMapperData(GarType.ADDR_OBJ_DIVISION),
+            new StAXAttrMapperData(GarType.ADDR_OBJ_TYPES),
+            new StAXAttrMapperData(GarType.ADM_HIERARCHY, BOOL_ACTIVE),
+            new StAXAttrMapperData(GarType.APARTMENT_TYPES),
+            new StAXAttrMapperData(GarType.APARTMENTS, BOOL_ACTUAL_ACTIVE),
+            new StAXAttrMapperData(GarType.OPERATION_TYPES),
+            new StAXAttrMapperData(GarType.CARPLACES, BOOL_ACTUAL_ACTIVE),
+            new StAXAttrMapperData(GarType.CHANGE_HISTORY),
+            new StAXAttrMapperData(GarType.HOUSE_TYPES),
+            new StAXAttrMapperData(GarType.HOUSES, BOOL_ACTUAL_ACTIVE),
+            new StAXAttrMapperData(GarType.MUN_HIERARCHY, BOOL_ACTIVE),
+            new StAXAttrMapperData(GarType.NORMATIVE_DOCS),
+            new StAXAttrMapperData(GarType.NORMATIVE_DOCS_KINDS),
+            new StAXAttrMapperData(GarType.NORMATIVE_DOCS_TYPES),
+            new StAXAttrMapperData(GarType.OBJECT_LEVELS),
+            new StAXAttrMapperData(GarType.PARAM_TYPES),
+            new StAXAttrMapperData(GarType.ADDR_OBJ_PARAMS),
+            new StAXAttrMapperData(GarType.HOUSES_PARAMS),
+            new StAXAttrMapperData(GarType.APARTMENTS_PARAMS),
+            new StAXAttrMapperData(GarType.ROOMS_PARAMS),
+            new StAXAttrMapperData(GarType.STEADS_PARAMS),
+            new StAXAttrMapperData(GarType.CARPLACES_PARAMS),
+            new StAXAttrMapperData(GarType.REESTR_OBJECTS, BOOL_ACTIVE),
+            new StAXAttrMapperData(GarType.ROOM_TYPES),
+            new StAXAttrMapperData(GarType.ROOMS, BOOL_ACTUAL_ACTIVE),
+            new StAXAttrMapperData(GarType.STEADS, BOOL_ACTUAL_ACTIVE),
+            new StAXAttrMapperData(GarType.ADDHOUSE_TYPES))
+            .collect(Collectors.toMap(StAXAttrMapperData::getGarType, Functions.identity()));
 
     private StAXAttrMapperData(GarType garType,
                                BiFunction<String, String, String> valueProcessing) {
